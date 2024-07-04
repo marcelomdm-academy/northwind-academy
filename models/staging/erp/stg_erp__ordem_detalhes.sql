eb4b48066a7abd33341361922b2a1dab6b945649
marcelomdm.academy@gmail.com <marcelomdm.academy@gmail.com>
2024-07-04 21:26:33 +0000

    fatos vendas finalizada

--- models/staging/erp/stg_erp__ordem_detalhes.sql
+++ models/staging/erp/stg_erp__ordem_detalhes.sql
@@ -1,13 +1,13 @@
 with
     fonte_ordem_detalhes as (
         select
-            cast(ORDERID as int) as fk_pedido
-            , cast(PRODUCTID as int) as fk_produto
-            , cast(DISCOUNT as numeric(18,2)) as desconto_perc
-            , cast(UNITPRICE as numeric(18,2)) as preco_unitario
-            , cast(QUANTITY as int) as quantidade
+            cast(orderid as int) as fk_pedido
+            , cast(productid as int) as fk_produto
+            , cast(discount as numeric(18,2)) as desconto_perc
+            , cast(unitprice as numeric(18,2)) as preco_da_unidade
+            , cast(quantity as int) as quantidade
         from {{ source('erp', 'orderdetail') }}
     )
 
-    select *
-    from fonte_ordem_detalhes
+select *
+from fonte_ordem_detalhes
